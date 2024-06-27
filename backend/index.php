8fc6ef492c886ac230b43e55f70104d92ed9498c
creatoff <creatoff@gmail.com>
2024-06-28 00:31:05 +0300

    fix cyrillic symbols in log

--- backend/index.php
+++ backend/index.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-W
 $data = json_decode(file_get_contents('php://input'), true);
 
 if (json_last_error() === JSON_ERROR_NONE) {
-    $logEntry = date('Y-m-d H:i:s') . ' - ' . json_encode($data) . PHP_EOL;
+    $logEntry = date('Y-m-d H:i:s') . ' - ' . json_encode($data, JSON_UNESCAPED_UNICODE) . PHP_EOL;
     file_put_contents('/var/log/log.txt', $logEntry, FILE_APPEND);
     echo json_encode(['status' => 'success']);
 } else {
